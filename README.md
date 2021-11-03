b64e552a26934f4d7dc2357b57e71faec9119d84
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-11-04 07:26:59 +0900

    sync https://github.com/WillBooster/one-way-git-sync/commits/5f0ee9e41dedade7ecf49c0e342727cd0a3ba8a4
    Initialize one-way-git-sync by replacing all the files with those of https://github.com/WillBooster/sample-of-one-way-git-sync

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# sample-of-one-way-git-sync
-A sample repo for syncing another repo with one-way-git-sync
+# one-way-git-sync
+
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+:arrows_counterclockwise: A tool for synchronizing a destination git repository with a source git repository SAFELY.
+
+It provides three features:
+
+1. replace the files of a destination repository (`dest repo`) with those of a source repository (`src repo`),
+2. add a replacement commit (`sync commit`) which whose message has the history of the included commits in the src repo and
+3. detect a conflict when a dest repo has a commit which a src repo doesn't have.
+
+## How to Use
+
+### Initial Usage
+
+Since `one-way-git-sync` finds a sync commit from the commit history of the dest repo,
+you need to run `init` subcommand to add an initial sync commit
+by replacing all the files of the dest repo with those of src repo at first.
+The sample command is as follows:
+
+```
+yarn one-way-git-sync init \
+  -d https://github.com/WillBooster/sample-of-one-way-git-sync \
+  -p https://github.com/WillBooster/one-way-git-sync/commits/
+```
+
+### Usual Usage
+
+If the last commit in the destination repository is a sync commit,
+`one-way-git-sync` safely synchronize the destination repository with the source repository.
+The sample command is as follows:
+
+```
+yarn one-way-git-sync \
+  -d https://github.com/WillBooster/sample-of-one-way-git-sync \
+  -p https://github.com/WillBooster/one-way-git-sync/commits/
+```
+
+### How to Deal with Conflicts
+
+If the last commit in the destination repository isn't a sync commit and if it is not first time,
+the destination repository probably has a commit which doesn't exist in the source repository.
+You basically need to merge such commits in the source repository at first,
+then, you need to force synchronizing the destination repository with the source repository.
+The sample commands are as follows:
+
+1. `cd one-way-git-sync`
+2. `git remote add upstream https://github.com/WillBooster/sample-of-one-way-git-sync`
+3. `git merge --allow-unrelated-histories upstream/main`
+4. ```
+   yarn one-way-git-sync --force \
+     -d https://github.com/WillBooster/sample-of-one-way-git-sync \
+     -p https://github.com/WillBooster/one-way-git-sync/commits/
+   ```
+
+## Example Repository and Example GitHub Actions Workflows
+
+[sample-of-one-way-git-sync](https://github.com/WillBooster/sample-of-one-way-git-sync) is an example synchronized repository.
+[release.yml](.github/workflows/release.yml) workflow releases a new npm package and add a tag automatically in this repository using `semantic-release`.
+When a new version is released, [sync.yml](.github/workflows/sync.yml) workflow synchronizes `sample-of-one-way-git-sync` repository using `one-way-git-sync`.
+We can manually trigger [force-sync.yml](.github/workflows/force-sync.yml) workflow which forces synchronizing `sample-of-one-way-git-sync` repository.
---
+++ src/index.ts
@@ -0,0 +1,143 @@
+import child_process from 'child_process';
+import fsp from 'fs/promises';
+import path from 'path';
+
+import fse from 'fs-extra';
+import simpleGit, { SimpleGit } from 'simple-git';
+import type { LogResult } from 'simple-git/typings/response';
+import { InferredOptionTypes } from 'yargs';
+
+import { logger } from './logger';
+import { yargsOptions } from './yargsOptions';
+
+const syncDirPath = path.join('node_modules', '.temp', 'sync-git-repo');
+const ignoreNames = ['.git', 'node_modules'];
+
+export async function sync(opts: InferredOptionTypes<typeof yargsOptions>, init: boolean): Promise<void> {
+  await fsp.mkdir(syncDirPath, { recursive: true });
+  const dirPath = await fsp.mkdtemp(path.join(syncDirPath, 'repo-'));
+  const ret = await syncCore(dirPath, opts, init);
+  await fsp.rm(dirPath, { recursive: true, force: true });
+  process.exit(ret ? 0 : 1);
+}
+
+async function syncCore(
+  destRepoPath: string,
+  opts: InferredOptionTypes<typeof yargsOptions>,
+  init: boolean
+): Promise<boolean> {
+  await simpleGit().clone(opts.dest, destRepoPath, opts.force ? undefined : { '--depth': 1 });
+  logger.verbose('Cloned a destination repo');
+
+  const dstGit: SimpleGit = simpleGit(destRepoPath);
+  if (opts.branch) {
+    try {
+      await dstGit.checkout(opts.branch);
+    } catch (_) {
+      await dstGit.checkoutLocalBranch(opts.branch);
+    }
+  }
+  const dstLog = await dstGit.log();
+
+  let from: string | undefined;
+  if (!init) {
+    from = extractCommitHash(dstLog);
+    if (!from) {
+      logger.error('No valid commit in destination repo');
+      return false;
+    }
+    logger.verbose(`Extracted a valid commit: ${from}`);
+  }
+
+  const srcGit: SimpleGit = simpleGit();
+  let srcLog: LogResult;
+  try {
+    // '--first-parent' hides children commits of merge commits
+    srcLog = await srcGit.log(from ? { from, to: 'HEAD', '--first-parent': undefined } : undefined);
+  } catch (e) {
+    logger.error('Failed to get source commit history:', e);
+    return false;
+  }
+
+  const latestHash = srcLog.latest?.hash;
+  if (!latestHash) {
+    logger.info('No synchronizable commit');
+    return true;
+  }
+
+  const [destFiles, srcFiles] = await Promise.all([fsp.readdir(destRepoPath), fsp.readdir('.')]);
+  for (const destFile of destFiles) {
+    if (ignoreNames.includes(destFile)) continue;
+    await fsp.rm(path.join(destRepoPath, destFile), { recursive: true, force: true });
+  }
+  for (const srcFile of srcFiles) {
+    if (ignoreNames.includes(srcFile)) continue;
+    fse.copySync(srcFile, path.join(destRepoPath, srcFile));
+  }
+  await dstGit.add('-A');
+
+  let srcTag = '';
+  if (opts['tag-hash'] || opts['tag-version']) {
+    // e.g. `--abbrev=0` changes `v1.31.5-2-gcdde507` to `v1.31.5`
+    const describeCommand = `git describe --tags --always ${opts['tag-version'] ? '--abbrev=0' : ''}`;
+    srcTag = child_process.execSync(describeCommand).toString().trim();
+  }
+  const link = `${opts.prefix || ''}${latestHash}`;
+  const title = srcTag ? `sync ${srcTag} (${link})` : `sync ${link}`;
+  const body = init
+    ? `Initialize one-way-git-sync by replacing all the files with those of ${opts.dest}`
+    : srcLog.all.map((l) => `* ${l.message}`).join('\n\n');
+  try {
+    await dstGit.commit(`${title}\n\n${body}`);
+    logger.verbose(`Created a commit: ${title}`);
+    logger.verbose(`  with body: ${body}`);
+  } catch (e) {
+    logger.error('Failed to commit changes:', e);
+    return false;
+  }
+
+  const destTag = srcTag || opts.tag;
+  if (destTag) {
+    try {
+      await dstGit.addTag(destTag);
+      logger.verbose(`Created a tag: ${destTag}`);
+    } catch (e) {
+      logger.error('Failed to commit changes:', e);
+      return false;
+    }
+  }
+
+  if (opts.dry) {
+    logger.verbose('Finished dry run');
+    return true;
+  }
+
+  try {
+    await dstGit.push();
+    if (destTag) {
+      await dstGit.push({ '--tags': null });
+    }
+  } catch (e) {
+    logger.error('Failed to push a commit:', e);
+    return false;
+  }
+
+  logger.verbose('Pushed a commit');
+  return true;
+}
+
+function extractCommitHash(logResult: LogResult): string | undefined {
+  if (logResult.all.length === 0) {
+    logger.error('No commit history');
+    return;
+  }
+
+  for (const log of logResult.all) {
+    const [head, ...words] = log.message.replace(/[()]/g, '').split(/[\s/]/);
+    if (head === 'sync' && words.length) {
+      return words[words.length - 1];
+    }
+  }
+  logger.error('No sync commit: ', logResult.all[0]);
+  return;
+}
